a700552242a92ebc04416203338f530af624204e
fstetler <fredrik_stetler@hotmail.com>
2019-10-24 19:59:54 +0200

    Final fix Password_Generator

--- Password_Generator.py
+++ Password_Generator.py
@@ -7,6 +7,8 @@ Extra: Ask the user how strong they want their password to be. For weak password
 
 def random(x):
 	
+	# Define lists of capital letters, lower case letters, and numbers
+
 	smalletter = list(string.ascii_lowercase)
 	bigletter = list(string.ascii_uppercase)
 	number = list(range(0,10))
@@ -15,6 +17,8 @@ def random(x):
 	password = []
 
 	start = 0
+
+	# For the length of the password, randomize either a capital, lower, or number in each item
 
 	while start <= x-1:
 
@@ -29,19 +33,25 @@ def random(x):
 	return password
 
 
-
 import string
+
+# Continue to run while generating is True
+
 Generating = True
 
 while Generating:
 
-	word = str(input("Lets create a password. Do you want to create one yourself (press 1) or generate one (press 2)?: \n"))
+	word = str(input("Let's create a password. Do you want to create one yourself (press 1) or generate one (press 2)?: \n"))
+
+	# If 1, wrote your own word as a password
 
 	if word == "1":
 		pw_self = str(input("Write what you want your password to be: \n"))
 		print("Your password is " + pw_self)
 		Generating = False
 
+	# If 2, decide the length of your password
+	
 	elif word == "2":
 		
 		length = int(input("How long do you want the password to be? \n"))
@@ -52,5 +62,10 @@ while Generating:
 		print("Your password is now " + pw_joined)
 
 		Generating = False
+
+	# If neither 1 or 2, repeat the process
+
+	else:
+		print("That is not an option. \n")
 
 
