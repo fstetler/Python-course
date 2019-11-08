b9ce688ee8651f03a08a94639a1f5ca65bd8d2b1
fstetler <fredrik_stetler@hotmail.com>
2019-11-08 18:41:58 +0100

    fixed space invaders

--- Space Invaders/Space_Invaders.py
+++ Space Invaders/Space_Invaders.py
@@ -5,18 +5,33 @@ pygame.init()
 
 # Create a display
 
-screen = pygame.display.set_mode((640,640))
+screenWidth = 640
+screen = pygame.display.set_mode((screenWidth,screenWidth))
+pygame.display.set_caption('Space Invaders')
 
 background = pygame.image.load('Background.png')
 ship = pygame.image.load('Ship.png')
 
-class dimensions(object):
+class dimensions():
 	def __init__(self,x,y,width,height):
 		self.x = x
 		self.y = y
 		self.width = width
 		self.height = height
-		self.vel = 2
+		self.speed = 2
+
+class shots():
+	def __init__(self,x,y,radius,color):
+		self.x = x
+		self.y = y
+		self.width = width
+		self.height = height
+		self.color = color
+		self.speed = 10
+
+	def draw(screen):
+		pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+
 
 
 def printScreen():
@@ -25,23 +40,26 @@ def printScreen():
 	pygame.display.update()
 
 
-shipStats = dimensions(300,400,64,64)
+shipStats = dimensions(300,400,128,128)
+projectiles = []
 run = True
 while run:
 
+
+
 	key = pygame.key.get_pressed()
 
-	if key[pygame.K_LEFT]:
-		shipStats.x -= shipStats.vel
+	if key[pygame.K_LEFT] and shipStats.x > shipStats.speed:
+		shipStats.x -= shipStats.speed
 
-	if key[pygame.K_RIGHT]:
-		shipStats.x += shipStats.vel
+	if key[pygame.K_RIGHT] and shipStats.x < screenWidth - shipStats.width - shipStats.speed:
+		shipStats.x += shipStats.speed
 
-	if key[pygame.K_UP]:
-		shipStats.y -= shipStats.vel
+	if key[pygame.K_UP] and shipStats.y > shipStats.speed:
+		shipStats.y -= shipStats.speed
 
-	if key[pygame.K_DOWN]:
-		shipStats.y += shipStats.vel
+	if key[pygame.K_DOWN] and shipStats.y < screenWidth - shipStats.height - shipStats.speed:
+		shipStats.y += shipStats.speed
 
 
 	for event in pygame.event.get():
